3eeeb27f34df2c71afab3109e735a19e748b9e3e
Richard Reynolds <richard@nautoguide.com>
2016-08-26 17:24:29 +0100

    Added d3 builder. Move update to separate script

---
+++ scripts/update.js
@@ -0,0 +1,109 @@
+const exec = require('child_process').exec;
+
+
+/**
+ *  Copy in the latest libs (update with npm install)
+ */
+exec('/bin/cp node_modules/jquery/dist/jquery.js js/libs/jquery', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+exec('/bin/cp node_modules/jquery/dist/jquery.min.js js/libs/jquery', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/document-register-element/build/document-register-element.js js/libs/register-element', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+exec('/bin/cp node_modules/document-register-element/build/document-register-element.max.js js/libs/register-element', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+exec('/bin/cp node_modules/dom4/build/dom4.js js/libs/dom4', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+exec('/bin/cp node_modules/dom4/build/dom4.max.js js/libs/dom4', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/particles.js/particles.js js/libs/particles', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/bowser/bowser.js js/libs/bowser', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/bowser/bowser.min.js js/libs/bowser', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/three/build/three.js js/libs/three', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/three/build/three.min.js js/libs/three', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+exec('/bin/cp node_modules/three/examples/js/controls/TrackballControls.js js/libs/three/plugins', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/openlayers/dist/ol.js js/libs/openlayers', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/openlayers/dist/ol-debug.js js/libs/openlayers', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp -r node_modules/ckeditor/* js/libs/ckeditor', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/bootstrap/dist/js/bootstrap* js/libs/bootstrap', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/d3/build/d3.js js/libs/d3', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
+exec('/bin/cp node_modules/d3/build/d3.min.js js/libs/d3', function(error, stdout, stderr) {
+    if(error) {
+        console.error(error);
+    }
+});
+
