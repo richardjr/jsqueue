85fd65d0fbe3fa42759447c389dd4e9632bb73be
ng-dan <dan.jones@nautoguide.com>
2016-11-22 11:11:41 +0000

    Fixed reports not wrapping

--- js/plugins/jsqueue-charts.js
+++ js/plugins/jsqueue-charts.js
@@ -40,33 +40,36 @@ function jsqueue_charts() {
             console.error('jsqueue charts cant find the chart structure');
         }
         jsqueue.finished(data.PID);
-    },
+    }
 
-     this.wrap=function(text, width) {
-         if(width<80)
-             width=80;
-             text.each(function () {
-                 var text = d3.select(this),
-                     words = text.text().split(/\s+/).reverse(),
-                     word,
-                     line = [],
-                     lineNumber = 0,
-                     lineHeight = 1.1, // ems
-                     y = text.attr("y"),
-                     dy = parseFloat(text.attr("dy")),
-                     tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-                 while (word = words.pop()) {
-                     line.push(word);
-                     tspan.text(line.join(" "));
-                     if (tspan.node().getComputedTextLength() > width) {
-                         line.pop();
-                         tspan.text(line.join(" "));
-                         line = [word];
-                         tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-                     }
-                 }
-             });
-        }
+    this.wrap = function(text, width) {
+        width = 80;
+
+        text.each(function() {
+            var text = d3.select(this),
+                words = text.text().split(/\s+/).reverse(),
+                word,
+                line = [],
+                lineNumber = 0,
+                lineHeight = 1.1, // ems
+                y = text.attr("y"),
+                dy = parseFloat(text.attr("dy")),
+                tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+
+            while (word = words.pop()) {
+                line.push(word);
+
+                tspan.text(line.join(" "));
+                
+                if (tspan.node().getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                }
+            }
+        });
+    };
 
     /**
      *  Make a table using a template
